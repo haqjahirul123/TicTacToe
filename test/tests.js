c9b63450d6fb6f6674414b5a189702ce21f2427b
Haqjahirul123 <haqjahirul1983@gmail.com>
2022-02-09 01:55:47 +0100

    test

--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,4 @@
-//const describe = require('describe');
-
- //import { winnerSelectY } from '../src/main'
- //import main from '../src/main';
-//  import expect from 'chai'
- const chai = window.chai
+const chai = window.chai
 const expect = chai.expect
 
 var x_Winner=parseInt(localStorage.getItem("scoreX"))
@@ -24,8 +19,8 @@ describe('winnerO', () => {
 describe('winnerX', () => {
   it('Check how many time "X"player winner', () => {
    
-        expect(x_Winner).to.deep.equal(parseInt(3))
-        //expect(x_Winner).to.deep.equal(parseInt(2))
+       // expect(x_Winner).to.deep.equal(parseInt(1))
+        expect(x_Winner).to.deep.equal(parseInt(2))
    
   })
 })
